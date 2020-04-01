5212ae3146642bf9587f6367d57358460092c2c3
Wes Morgan <wesmorgan@icloud.com>
2020-04-01 10:49:40 -0600

    Add a whoami subcommand to get your username
    These come from the git config so they can vary per repo

---
+++ cmd/whoami.go
@@ -0,0 +1,35 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(whoAmICommand)
+}
+
+var whoAmICommand = &cobra.Command{
+	Use: "whoami",
+	Short: "Print out your username in the current repo",
+	Args: cobra.NoArgs,
+	Run: func(cmd *cobra.Command, args []string) {
+		callingDir, err := os.Getwd()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "error getting current workdir: %w", err)
+			os.Exit(1)
+		}
+
+		repo := openRepo(cmd, callingDir)
+
+		username, err := repo.Username()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+
+		fmt.Println(username)
+	},
+}
